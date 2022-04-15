607acdb7e126582c94e2a8fe46406e4626bc5289
koji <brienjayke1901@gmail.com>
2022-04-15 22:13:41 +0100

    <3

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+const Main = () => {
+    return (
+        <div className="Main">
+            {/*
+            <h3>Hi, I'm <span>J</span>ayke;</h3>
+            <h4>Software Developer.</h4>
+            */}
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+    return (
+        <div className="Sidebar">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Tools</a></li>
+                <li><a href="/">Projects</a></li>
+                <li><a href="/">Socials</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
